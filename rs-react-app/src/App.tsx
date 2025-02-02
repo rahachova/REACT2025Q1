9d6b3fbf3bbdac82c1e51ee37b0aaa96bce432c4
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2025-02-02 18:28:00 +0100

    feat: add a button to throw an error

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,11 +1,29 @@
 import { Component } from 'react';
 import './App.css';
 
+interface IState {
+  hasError: boolean;
+}
+
 class App extends Component {
+  state: IState = {
+    hasError: false,
+  };
+
+  throwError = () => {
+    this.setState(({ hasError }: IState) => ({
+      hasError: !hasError,
+    }));
+  };
+
   render() {
+    if (this.state.hasError) {
+      throw Error('Error!');
+    }
+
     return (
       <>
-        <h1>Hello</h1>
+        <button onClick={this.throwError}>Throw error</button>
       </>
     );
   }
