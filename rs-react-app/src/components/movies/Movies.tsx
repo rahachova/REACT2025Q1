e1cf3fb08c374c5c1cc5106648c2a78782015d6c
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2025-02-22 18:17:54 +0100

    feat: selected items management

--- rs-react-app/src/components/movies/Movies.tsx
+++ rs-react-app/src/components/movies/Movies.tsx
@@ -1,8 +1,11 @@
-import { useCallback, useMemo } from 'react';
+import { ChangeEvent, useCallback, useMemo } from 'react';
 import { IMovie } from '../../types/movie';
 import { useSearchParams } from 'react-router';
-import './Movies.css';
 import { Spinner } from '../spinner/Spinner';
+import { select, remove } from '../../store/selected-movie-slice';
+import { useDispatch, useSelector } from 'react-redux';
+import './Movies.css';
+import { RootState } from '../../store/store';
 
 interface IProps {
   movies: IMovie[];
@@ -11,8 +14,10 @@ interface IProps {
 
 export function Movies({ movies, isLoading }: IProps) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { selectedMovies } = useSelector((state: RootState) => state);
+  const dispatch = useDispatch();
 
-  const handleMovieSelect = useCallback(
+  const handleMovieActivate = useCallback(
     (uid: string) => {
       searchParams.set('details', uid);
       setSearchParams(searchParams);
@@ -20,34 +25,69 @@ export function Movies({ movies, isLoading }: IProps) {
     [searchParams, setSearchParams]
   );
 
+  const handleMovieSelect = useCallback(
+    (movie: IMovie, isSelected: boolean) => {
+      if (isSelected) {
+        dispatch(select(movie));
+      } else {
+        dispatch(remove(movie.uid));
+      }
+    },
+    [dispatch]
+  );
+
+  const isMovieSelected = useCallback(
+    (uid: string) => {
+      return selectedMovies.some((movie) => movie.uid === uid);
+    },
+    [selectedMovies]
+  );
+
   const moviesTable = useMemo(() => {
     return movies.length ? (
       <table className="movies">
         <thead>
           <tr>
+            <th></th>
             <th>Title</th>
             <th>Release Date</th>
             <th>Main Director</th>
           </tr>
         </thead>
         <tbody>
-          {movies.map(({ uid, title, usReleaseDate, mainDirector }) => (
-            <tr
-              onClick={() => handleMovieSelect(uid)}
-              key={uid}
-              className="movie"
-            >
-              <td>{title}</td>
-              <td>{usReleaseDate}</td>
-              <td>{mainDirector.name}</td>
-            </tr>
-          ))}
+          {movies.map((movie) => {
+            const { uid, title, usReleaseDate, mainDirector } = movie;
+
+            return (
+              <tr
+                onClick={() => handleMovieActivate(uid)}
+                key={uid}
+                className="movie"
+              >
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={isMovieSelected(uid)}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      handleMovieSelect(movie, e.target.checked);
+                    }}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  />
+                </td>
+                <td>{title}</td>
+                <td>{usReleaseDate}</td>
+                <td>{mainDirector.name}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     ) : (
       <div>No movies with such name</div>
     );
-  }, [handleMovieSelect, movies]);
+  }, [handleMovieActivate, handleMovieSelect, isMovieSelected, movies]);
 
   return (
     <div className="movies_container">
--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import { App } from './App.tsx';
 import { ErrorBoundary } from './components/error-boundary/ErrorBoundary.tsx';
 import { BrowserRouter } from 'react-router';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
